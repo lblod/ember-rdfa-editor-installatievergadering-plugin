1091b6e383021c1a97a0c7b9159780df741909eb
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-11-23 17:52:53 +0100

    added timstamp to trigger diff

--- addon/components/burgemeester-aanstelling/no-burgemeester-template.js
+++ addon/components/burgemeester-aanstelling/no-burgemeester-template.js
@@ -2,5 +2,9 @@ import Component from '@ember/component';
 import layout from '../../templates/components/burgemeester-aanstelling/no-burgemeester-template';
 
 export default Component.extend({
-  layout
+  layout,
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('timestampToTriggerDiff', new Date().toISOString());
+  }
 });
