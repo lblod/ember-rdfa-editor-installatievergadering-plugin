29d8dbfc97b284179ac87f07fe01b6c880fc219e
Erika Pauwels <erika.pauwels@gmail.com>
2018-11-27 12:40:46 +0100

    Add missing component files

---
+++ addon/components/burgemeester-aanstelling/burgemeester-no-oath-template.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../templates/components/burgemeester-aanstelling/burgemeester-no-oath-template';
+
+export default Component.extend({
+  layout
+});
