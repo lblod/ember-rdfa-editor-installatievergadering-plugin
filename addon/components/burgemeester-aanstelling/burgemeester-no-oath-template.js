cc29f352771625f53aac9e386782f1a53b201824
Erika Pauwels <erika.pauwels@gmail.com>
2018-11-27 13:05:08 +0100

    Update templates to insert for burgemeester benoeming

--- addon/components/burgemeester-aanstelling/burgemeester-no-oath-template.js
+++ addon/components/burgemeester-aanstelling/burgemeester-no-oath-template.js
@@ -1,6 +1,17 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/burgemeester-aanstelling/burgemeester-no-oath-template';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  layout
+  layout,
+
+  burgemeesterPersoonId1: computed('elementId', function() {
+    return `burgemeester-persoon-1-${this.elementId}`;
+  }),
+  burgemeesterPersoonId2: computed('elementId', function() {
+    return `burgemeester-persoon-2-${this.elementId}`;
+  }),
+  burgemeesterPersoonId3: computed('elementId', function() {
+    return `burgemeester-persoon-3-${this.elementId}`;
+  })
 });
--- addon/components/burgemeester-aanstelling/no-burgemeester-template.js
+++ addon/components/burgemeester-aanstelling/no-burgemeester-template.js
@@ -1,6 +1,11 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/burgemeester-aanstelling/no-burgemeester-template';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  layout
+  layout,
+
+  burgemeesterPersoonId: computed('elementId', function() {
+    return `burgemeester-persoon-1-${this.elementId}`;
+  })
 });
